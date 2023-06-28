6c60aa335f8f976c6986866047d59208136e6070
ysdnrm <133837502+ysdnrm@users.noreply.github.com>
2023-06-28 07:54:47 +0000

    [Finish] follow

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # get 'relationships/followings'
+  # get 'relationships/followers'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root :to =>"homes#top"
@@ -11,7 +13,12 @@ Rails.application.routes.draw do
   end
   
   devise_for :users
-  resources :users, only: [:index,:show,:edit,:update]
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+
+  end
  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
