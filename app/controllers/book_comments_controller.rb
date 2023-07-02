d5e44fc51a587318e7925680c67841707a71c390
ysdnrm <133837502+ysdnrm@users.noreply.github.com>
2023-07-02 07:45:16 +0000

    [Finish] 非同期処理

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,17 +1,22 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_back(fallback_location: root_path)
+    render :book_comments
+    # render 'create.js.erb'
+    # redirect_back(fallback_location: root_path)
     # redirect_to book_path(book)
   end
 
   def destroy
     BookComment.find(params[:id]).destroy
-    redirect_back(fallback_location: root_path)
+    # jsファイルで@bookを書いているから、ここで定義が必要
+    @book = Book.find(params[:book_id])
+    render :book_comments
+    # redirect_back(fallback_location: root_path)
     # redirect_to book_path(params[:book_id])
   end
 
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,16 @@
 class FavoritesController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
+
     # 非同期処理のため消す。redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
     # 非同期処理のため消す。redirect_back(fallback_location: root_path)
   end
