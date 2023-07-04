4696ba9b605ad80d14025d2540967ef65d1d77bd
ysdnrm <133837502+ysdnrm@users.noreply.github.com>
2023-07-04 12:56:09 +0000

    [Add] グループ機能（メンバーの参加と退会）

--- app/controllers/group_users_controller.rb
+++ app/controllers/group_users_controller.rb
@@ -3,6 +3,7 @@ class GroupUsersController < ApplicationController
   
   # グループに参加
   def create 
+    # group.users_id << ouner_user_id
     group_user = current_user.group_users.new(group_id: params[:group_id])
     group_user.save
     # 前いたページに戻る
@@ -11,6 +12,7 @@ class GroupUsersController < ApplicationController
   
   # グループを退会
   def destroy 
+    # find_by は id 以外の検索で使用(今回は group_idでただのidでないので使う)
     group_user = current_user.group_users.find_by(group_id: params[:group_id])
     group_user.destroy
     redirect_to request.referer
